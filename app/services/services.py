bdfda9fc123b07480ea9a78978818be9104a8990
hosseink9 <hosseinkalantari.29@gmail.com>
2023-11-02 15:56:37 +0330

    Create verify password func

--- app/services/services.py
+++ app/services/services.py
@@ -25,3 +25,6 @@ class UserService:
     def get_hashed_password(self, password: str) -> str:
         return self.password_context.hash(password)
 
+    def verify_password(self, password: str, hashed_pass: str) -> bool:
+        return self.password_context.verify(password, hashed_pass)
+
