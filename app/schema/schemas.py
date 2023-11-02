6c7d69f7d433087f7461707803312191168dee96
hosseink9 <hosseinkalantari.29@gmail.com>
2023-11-02 15:52:15 +0330

    Add schemas

---
+++ app/schema/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+
+class UserRequest(BaseModel):
+    username: str
+    password: str
+    firstname: str | None=None
+    lastname: str | None=None
+
+
+class UserResponse(BaseModel):
+    id: str
+    username: str
+    firstname: str | None=None
+    lastname: str | None=None
+
+
+
+class UserUpdate(BaseModel):
+    username: str | None=None
+    firstname: str | None=None
+    lastname: str | None=None
